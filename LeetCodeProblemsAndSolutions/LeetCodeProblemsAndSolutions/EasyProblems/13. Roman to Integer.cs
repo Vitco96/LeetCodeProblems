7ecdbed659aa00490321ca364b4ad40da84ee56f
Vitcovschi Mihai <mihai.vitcovschi@miele.com>
2023-10-12 10:14:46 +0300

    Added problem number 13

---
+++ LeetCodeProblemsAndSolutions/LeetCodeProblemsAndSolutions/EasyProblems/13. Roman to Integer.cs
@@ -0,0 +1,103 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LeetCodeProblemsAndSolutions.EasyProblems;
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+//
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+//
+//Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+//
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+//
+//
+//
+//     Example 1:
+//
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+//     Example 2:
+//
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+//     Example 3:
+//
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+//
+//
+//     Constraints:
+//
+// 1 <= s.length <= 15
+// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+public static class Roman_to_Integer
+{
+    //In the first approach based on the possible cases where we substract the lesser number from the bigger one,
+    //we replace it and by iterating the string, we add each value in the dictionary.
+    public static int RomanToInt(string s)
+    {
+        Dictionary<char, int> romanValues = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        var total = 0;
+        s = s.Replace("IV", "IIII");
+        s = s.Replace("IX", "VIIII");
+        s = s.Replace("XL", "XXXX");
+        s = s.Replace("XC", "LXXXX");
+        s = s.Replace("CD", "CCCC");
+        s = s.Replace("CM", "DCCCC");
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            total += romanValues[s[i]];
+        }
+
+        return total;
+    }
+    public static int RomanToIntDictionary(string s)
+    {
+        Dictionary<char, int> romanValues = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        var total = 0;
+
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            if (romanValues[s[i]] < romanValues[s[i + 1]]) total -= romanValues[s[i]];
+            else total += romanValues[s[i]];
+        }
+
+        return total + romanValues[s[s.Length-1]];
+    }
+}
--- LeetCodeProblemsAndSolutions/LeetCodeProblemsAndSolutions/Program.cs
+++ LeetCodeProblemsAndSolutions/LeetCodeProblemsAndSolutions/Program.cs
@@ -6,8 +6,12 @@ namespace LeetCodeProblemsAndSolutions
     {
         static void Main(string[] args)
         {
-            var a = 100;
-            FizzBuzz.RunMethod(a);
+            // var a = 100;
+            // FizzBuzz.RunMethod(a);
+
+            var romanString = "MCMXIV";
+            Console.WriteLine(Roman_to_Integer.RomanToIntDictionary(romanString));
+            Console.WriteLine(Roman_to_Integer.RomanToInt(romanString));
         }
     }
 }
