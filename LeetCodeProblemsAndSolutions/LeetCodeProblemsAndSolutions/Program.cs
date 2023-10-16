3e81f8bfbe709967021f9ae601809b169d8d5d38
Vitcovschi Andrei <vitcovschi86@gmail.com>
2023-10-16 11:16:53 +0300

    Added a medium problem with the linked list

--- LeetCodeProblemsAndSolutions/LeetCodeProblemsAndSolutions/Program.cs
+++ LeetCodeProblemsAndSolutions/LeetCodeProblemsAndSolutions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCodeProblemsAndSolutions.EasyProblems;
+using LeetCodeProblemsAndSolutions.MediumProblems;
 
 namespace LeetCodeProblemsAndSolutions
 {
@@ -9,9 +10,15 @@ namespace LeetCodeProblemsAndSolutions
             // var a = 100;
             // FizzBuzz.RunMethod(a);
 
-            var romanString = "MCMXIV";
-            Console.WriteLine(Roman_to_Integer.RomanToIntDictionary(romanString));
-            Console.WriteLine(Roman_to_Integer.RomanToInt(romanString));
+            //var romanString = "MCMXIV";
+            //Console.WriteLine(Roman_to_Integer.RomanToIntDictionary(romanString));
+            //Console.WriteLine(Roman_to_Integer.RomanToInt(romanString));
+
+            LinkedList<int> firstNumber = new LinkedList<int>(new int[] { 2, 4, 3 });
+            LinkedList<int> secondNumber = new LinkedList<int>(new int[] { 5, 6, 4});
+            var list = AddTwoNumbersProblem.AddTwoNumbers(firstNumber, secondNumber);
+            Console.WriteLine("Result : ");
+            foreach(int i in list) { Console.WriteLine(i); }
         }
     }
 }
