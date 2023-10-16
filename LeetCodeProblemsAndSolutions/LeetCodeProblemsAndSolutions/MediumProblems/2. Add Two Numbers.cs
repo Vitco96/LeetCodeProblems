e32a9e16e3a1a665803b766302e00af7d5f88c13
Vitcovschi Andrei <vitcovschi86@gmail.com>
2023-10-16 12:09:27 +0300

    Added LongestSubstring Problem

--- LeetCodeProblemsAndSolutions/LeetCodeProblemsAndSolutions/MediumProblems/2. Add Two Numbers.cs
+++ LeetCodeProblemsAndSolutions/LeetCodeProblemsAndSolutions/MediumProblems/2. Add Two Numbers.cs
@@ -6,11 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace LeetCodeProblemsAndSolutions.MediumProblems
-{
- //   You are given two non-empty linked lists representing two non-negative integers.The digits are stored in reverse order, and each of their nodes contains a single digit.Add the two numbers and return the sum as a linked list.
- //   You may assume the two numbers do not contain any leading zero, except the number 0 itself.
- //   Example 1:
+//   You are given two non-empty linked lists representing two non-negative integers.The digits are stored in reverse order, and each of their nodes contains a single digit.Add the two numbers and return the sum as a linked list.
+//   You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//   Example 1:
 
 
 //Input: l1 = [2, 4, 3], l2 = [5, 6, 4]
@@ -33,22 +31,28 @@ namespace LeetCodeProblemsAndSolutions.MediumProblems
 //It is guaranteed that the list represents a number that does not have leading zeros.
 
 
+
+namespace LeetCodeProblemsAndSolutions.MediumProblems
+{
     public static class AddTwoNumbersProblem
     {
+
+        //First solution is to use the LinkedList feature of .NET and create two int numbers from the linked list, add them 
+        //and after we find out the result, we create a new linked list 
         public static LinkedList<int> AddTwoNumbers(LinkedList<int> l1, LinkedList<int> l2)
         {
             LinkedList<int> resultList = new LinkedList<int>();
             int index1 = 1, index2 = 1;
             int num1 = 0, num2 = 0;
 
-            while(l1.Count != 0)
+            while (l1.Count != 0)
             {
                 num1 += l1.First() * index1;
                 index1 *= 10;
                 l1.RemoveFirst();
             }
 
-            while(l2.Count != 0)
+            while (l2.Count != 0)
             {
                 num2 += l2.First() * index2;
                 index2 *= 10;
@@ -57,13 +61,50 @@ namespace LeetCodeProblemsAndSolutions.MediumProblems
 
             var result = num1 + num2;
 
-            while(result > 0)
+            while (result > 0)
             {
                 resultList.AddLast(result % 10);
                 result = result / 10;
             }
 
             return resultList;
+        }
+
+        
+        //THe second solution is a similar solution but instead of using .Net linked list, we use the example ListNode provided
+        public static ListNode AddTwoNumbersWithSpecificClass(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode();
+            ListNode head = result;
+            int sum = 0;
+            while (l1 != null || l2 != null || sum > 0)
+            {
+                if (l1 != null)
+                {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+                result.next = new ListNode(sum % 10);
+                sum /= 10;
+                result = result.next;
+            }
+            return head.next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
         }
     }
 }
--- LeetCodeProblemsAndSolutions/LeetCodeProblemsAndSolutions/Program.cs
+++ LeetCodeProblemsAndSolutions/LeetCodeProblemsAndSolutions/Program.cs
@@ -14,11 +14,14 @@ namespace LeetCodeProblemsAndSolutions
             //Console.WriteLine(Roman_to_Integer.RomanToIntDictionary(romanString));
             //Console.WriteLine(Roman_to_Integer.RomanToInt(romanString));
 
-            LinkedList<int> firstNumber = new LinkedList<int>(new int[] { 2, 4, 3 });
-            LinkedList<int> secondNumber = new LinkedList<int>(new int[] { 5, 6, 4});
-            var list = AddTwoNumbersProblem.AddTwoNumbers(firstNumber, secondNumber);
-            Console.WriteLine("Result : ");
-            foreach(int i in list) { Console.WriteLine(i); }
+            //LinkedList<int> firstNumber = new LinkedList<int>(new int[] { 2, 4, 3 });
+            //LinkedList<int> secondNumber = new LinkedList<int>(new int[] { 5, 6, 4});
+            //var list = AddTwoNumbersProblem.AddTwoNumbers(firstNumber, secondNumber);
+            //Console.WriteLine("Result : ");
+            //foreach(int i in list) { Console.WriteLine(i); }
+
+            var input = "abcabcbb";
+            Console.WriteLine(LongestSubstring.LengthOfLongestSubstring(input));
         }
     }
 }
